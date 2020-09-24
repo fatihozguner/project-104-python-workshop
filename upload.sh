06cce7e4b3529fffa79807a22ee83373340b9f41
Fatih <fatihozguner@hotmail.com>
2020-09-23 21:21:21 -0500

    files updated

--- upload.sh
+++ upload.sh
@@ -1,3 +1,5 @@
+aws s3 ls # should list all the buckets
+aws s3 ls | grep kittens # should list buckets containing "kittens" keyword
 aws s3 ls s3://kittens.fatih-us.com # should return empty
 aws s3 sync . s3://kittens.fatih-us.com
 aws s3 ls s3://kittens.fatih-us.com # should see list of website content
